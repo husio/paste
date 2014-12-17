f05eca7ac42f6fdae5512344d501c666e8f2aa5d
Piotr Husiatyński <phusiatynski@gmail.com>
2014-12-17 16:19:50 +0100

    old core removed

--- main.go
+++ main.go
@@ -3,20 +3,11 @@ package main
 import (
 	"flag"
 	"log"
-	"path"
-	"runtime"
 	"time"
 
 	"github.com/husio/paste/store"
 	"github.com/husio/paste/web"
 )
-
-var rootDir string
-
-func init() {
-	_, filename, _, _ := runtime.Caller(1)
-	rootDir = path.Dir(filename)
-}
 
 func main() {
 	var httpPort *int = flag.Int("port", 8000, "HTTP server port")
